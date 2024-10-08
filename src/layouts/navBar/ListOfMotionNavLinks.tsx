c51d02bcd01997f472f8683b5c66aeb999e88b7a
Martin Morales <martinexequielol@gmail.com>
2024-10-08 14:19:10 -0300

    add the traduction to the mobile menu

--- src/layouts/navBar/ListOfMotionNavLinks.tsx
+++ src/layouts/navBar/ListOfMotionNavLinks.tsx
@@ -1,12 +1,23 @@
+import { LANGUAGE } from "../../data/consts";
+import { useLanguage } from "../../hooks/useLanguage";
 import { MotionNavLink } from "./MotionNavLink";
 
 export function ListMotionNavLinks() {
+	const { language } = useLanguage();
 	return (
 		<>
-			<MotionNavLink link="#about-me">About Me</MotionNavLink>
-			<MotionNavLink link="#tech">Tecnologies</MotionNavLink>
-			<MotionNavLink link="#experience">Experience</MotionNavLink>
-			<MotionNavLink link="#contact">Contact</MotionNavLink>
+			<MotionNavLink link="#about-me">
+				{LANGUAGE[language].sections.about.title}
+			</MotionNavLink>
+			<MotionNavLink link="#tech">
+				{LANGUAGE[language].sections.technologies.title}
+			</MotionNavLink>
+			<MotionNavLink link="#experience">
+				{LANGUAGE[language].sections.experience.title}
+			</MotionNavLink>
+			<MotionNavLink link="#contact">
+				{LANGUAGE[language].sections.contact.title}
+			</MotionNavLink>
 		</>
 	);
 }
